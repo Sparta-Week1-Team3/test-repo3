d1efab951919daeecfe7cd388d8c272aec938224
kimminung <minogo1054@paran.com>
2024-03-08 17:08:29 +0900

    kimm 변경 수락제발요

--- git-test-playground.playground/Contents.swift
+++ git-test-playground.playground/Contents.swift
@@ -1,3 +1,49 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func doubleArray(_ num1:Int, _ num2:Int) -> [[Int]] {
+    
+
+    var count = 1
+    var arrB : [[Int]] = []
+
+    for j in 0..<num2 {
+        
+        var arr1 = [Int]()
+        
+        for i in 0..<num1 {
+            arr1.append(count*i)
+        }
+
+        arrB.append(arr1)
+ }
+    print(arrB)
+
+ return arrB
+}
+
+doubleArray(10, 10)
